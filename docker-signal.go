12f7773079f6eb1c91ce4850ac4fa69e7c86c3ad
John Howard <jhoward@microsoft.com>
2015-07-06 18:33:32 -0700

    Initial version
    Signed-off-by: John Howard <jhoward@microsoft.com>

---
+++ docker-signal.go
@@ -0,0 +1,66 @@
+package main
+
+// Very simple utility which signals an event. Used to signal a docker
+// daemon on Windows to dump its stacks. Usage docker-signal --pid=daemonpid
+
+import (
+	"flag"
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+const EVENT_MODIFY_STATUS = 0x0002
+
+var (
+	modkernel32    = syscall.NewLazyDLL("kernel32.dll")
+	procOpenEvent  = modkernel32.NewProc("OpenEventW")
+	procPulseEvent = modkernel32.NewProc("PulseEvent")
+)
+
+func OpenEvent(desiredAccess uint32, inheritHandle bool, name string) (handle syscall.Handle, err error) {
+	namep, _ := syscall.UTF16PtrFromString(name)
+	var _p2 uint32 = 0
+	if inheritHandle {
+		_p2 = 1
+	}
+	r0, _, e1 := procOpenEvent.Call(uintptr(desiredAccess), uintptr(_p2), uintptr(unsafe.Pointer(namep)))
+	use(unsafe.Pointer(namep))
+	handle = syscall.Handle(r0)
+	if handle == syscall.InvalidHandle {
+		err = e1
+	}
+	return
+}
+
+func PulseEvent(handle syscall.Handle) (err error) {
+	r0, _, _ := procPulseEvent.Call(uintptr(handle))
+	if r0 != 0 {
+		err = syscall.Errno(r0)
+	}
+	return
+}
+
+func main() {
+	var pid int
+	flag.IntVar(&pid, "pid", -1, "PID of docker daemon to signal to dump stacks")
+	flag.Parse()
+	if pid == -1 {
+		fmt.Println("Error: pid must be supplied")
+		return
+	}
+	ev := "Global\\docker-daemon-" + fmt.Sprint(pid)
+	h2, _ := OpenEvent(EVENT_MODIFY_STATUS, false, ev)
+	if h2 == 0 {
+		fmt.Printf("Could not open event. Check PID %d is correct and the daemon is running.\n", pid)
+		return
+	}
+	PulseEvent(h2)
+	fmt.Println("Daemon signalled successfully. Examine its output for stacks")
+}
+
+var temp unsafe.Pointer
+
+func use(p unsafe.Pointer) {
+	temp = p
+}
